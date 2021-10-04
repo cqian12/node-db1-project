7668814aa229ce22b7653218205f70585c289db8
Charles Qian <charleshqian@gmail.com>
2021-10-04 16:53:16 -0700

    completed MVP

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
-
+const aRouter = require('./accounts/accounts-router')
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', aRouter)
 
 module.exports = server;
